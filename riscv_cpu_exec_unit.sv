f592138627fe4b3432647d5a3fdef52d899d37f5
Saunders Riley <riley.saundersm@gmail.com>
2024-12-01 14:57:54 -0500

    More work on the ALU, initial work on the pipeline

--- riscv_cpu_exec_unit.sv
+++ riscv_cpu_exec_unit.sv
@@ -1,9 +1,125 @@
 `include "riscv_instruction_set.vh"
 
 module riscv_cpu_exec_unit(
+    // Core Signals
     input   logic       cpu_clk,
     input   logic       cpu_resetn,
+    // Pipeline Interface from Fetch
+    input   logic[31:0] pipe_in_instr,
+    input   logic[31:0] pipe_in_pcaddr,
+    input   logic       pipe_stall_in,
+    output  logic       fetch_stall_out,
+    output  logic       fetch_branch,
+    output  logic[31:0] fetch_branch_pcaddr,
+    // AHB Data Interface
+    // AHB HCLK and HRESETN should be connected to CPU_CLK and CPU_RESETN,
+    // respectively
+    output  logic[31:0] m_ahb_haddr,
+    output  logic[2:0]  m_ahb_hsize,
+    output  logic[1:0]  m_ahb_htrans,
+    output  logic[31:0] m_ahb_hwdata,
+    output  logic[3:0]  m_ahb_hwstrb,
+    output  logic       m_ahb_hwrite,
+    input   logic[31:0] m_ahb_hrdata,
+    input   logic       m_ahb_hreadyin,
+    input   logic       m_ahb_hresp,
 );
+    // Pipeline Stage 2 - Instruction Decode
+    logic[31:0]     pl_fetch_instr;
+    logic[31:0]     pl_fetch_pcaddr;
+    logic[6:0]      pl_fetch_funct7     = pl_fetch_instr[31:25];
+    logic[4:0]      pl_fetch_rs2        = pl_fetch_instr[24:20];
+    logic[4:0]      pl_fetch_rs1        = pl_fetch_instr[19:15];
+    logic[2:0]      pl_fetch_funct3     = pl_fetch_instr[14:12];
+    logic[4:0]      pl_fetch_rd         = pl_fetch_instr[11:7];
+    logic[6:0]      pl_fetch_opcode     = pl_fetch_instr[6:0];
+    logic[11:0]     pl_fetch_imm_I      = pl_fetch_instr[11:0];
+    logic[11:0]     pl_fetch_imm_S      = {pl_fetch_funct7, pl_fetch_rd};
+    logic[12:0]     pl_fetch_imm_B      = {pl_fetch_funct7[6], pl_fetch_rd[0], pl_fetch_funct7[5:0], pl_fetch_rd[4:1], 1'b0};
+    logic[19:0]     pl_fetch_imm_U      = pl_fetch_instr[31:12];
+    logic[20:0]     pl_fetch_imm_J      = {pl_fetch_imm_U[19], pl_fetch_imm_U[7:0], pl_fetch_imm_U[8], pl_fetch_imm_U[18:9], 1'b0};
+
+    // Pipeline Stage 3 - Instruction Execute
+    logic[31:0]     pl_exec_instr;
+    logic[31:0]     pl_exec_pcaddr;
+    logic[31:0]     pl_exec_op1;
+    logic[31:0]     pl_exec_op2;
+    logic[31:0]     pl_exec_op3;
+    logic[6:0]      pl_exec_funct7      = pl_exec_instr[31:25];
+    logic[4:0]      pl_exec_rs2         = pl_exec_instr[24:20];
+    logic[4:0]      pl_exec_rs1         = pl_exec_instr[19:15];
+    logic[2:0]      pl_exec_funct3      = pl_exec_instr[14:12];
+    logic[4:0]      pl_exec_rd          = pl_exec_instr[11:7];
+    logic[6:0]      pl_exec_opcode      = pl_exec_instr[6:0];
+    logic[11:0]     pl_exec_imm_I       = pl_exec_instr[11:0];
+    logic[11:0]     pl_exec_imm_S       = {pl_exec_funct7, pl_exec_rd};
+    logic[12:0]     pl_exec_imm_B       = {pl_exec_funct7[6], pl_exec_rd[0], pl_exec_funct7[5:0], pl_exec_rd[4:1], 1'b0};
+    logic[19:0]     pl_exec_imm_U       = pl_exec_instr[31:12];
+    logic[20:0]     pl_exec_imm_J       = {pl_exec_imm_U[19], pl_exec_imm_U[7:0], pl_exec_imm_U[8], pl_exec_imm_U[18:9], 1'b0};
+
+    logic[31:0]     pl_exec_op1_fwd;
+    logic[31:0]     pl_exec_op2_fwd;
+    logic[31:0]     pl_exec_op3_fwd;
+    logic[31:0]     alu_res;
+    logic[31:0]     alu_byp;
+
+    // Pipeline Stage 4 - Instruction Writeback
+    logic[31:0]     pl_wb_instr;
+    logic[31:0]     pl_wb_pcaddr;
+    logic[31:0]     pl_wb_res;
+    logic[31:0]     pl_wb_byp;
+    logic[6:0]      pl_wb_funct7        = pl_wb_instr[31:25];
+    logic[4:0]      pl_wb_rs2           = pl_wb_instr[24:20];
+    logic[4:0]      pl_wb_rs1           = pl_wb_instr[19:15];
+    logic[2:0]      pl_wb_funct3        = pl_wb_instr[14:12];
+    logic[4:0]      pl_wb_rd            = pl_wb_instr[11:7];
+    logic[6:0]      pl_wb_opcode        = pl_wb_instr[6:0];
+    logic[11:0]     pl_wb_imm_I         = pl_wb_instr[11:0];
+    logic[11:0]     pl_wb_imm_S         = {pl_wb_funct7, pl_wb_rd};
+    logic[12:0]     pl_wb_imm_B         = {pl_wb_funct7[6], pl_wb_rd[0], pl_wb_funct7[5:0], pl_wb_rd[4:1], 1'b0};
+    logic[19:0]     pl_wb_imm_U         = pl_wb_instr[31:12];
+    logic[20:0]     pl_wb_imm_J         = {pl_wb_imm_U[19], pl_wb_imm_U[7:0], pl_wb_imm_U[8], pl_wb_imm_U[18:9], 1'b0};
+
+    // Arithmetic Unit
+    riscv_cpu_alu alu_inst(
+        // Core Signals
+        .cpu_clk(cpu_clk),
+        .cpu_resetn(cpu_resetn),
+        // Control Inputs
+        .opcode(pl_exec_opcode),
+        .funct3(pl_exec_funct3),
+        .funct7(pl_exec_funct7),
+        // Operand Inputs
+        .op1(pl_exec_op1_fwd),
+        .op2(pl_exec_op2_fwd),
+        .op3(pl_exec_op3_fwd),
+        // Result Outputs
+        .res(alu_res),
+        .byp(alu_byp)
+    );
+
+    // Register File
+    riscv_cpu_regs regs_inst(
+        // Core Signals
+        .cpu_clk(),
+        .cpu_resetn(),
+        // Source Register 1
+        .rs1_sel(),
+        .rs1_data(),
+        // Source Register 2
+        .rs2_sel(),
+        .rs2_data(),
+        // Destination Register 1
+        .rd1_sel(),
+        .rd1_data(),
+        .rd1_wren(),
+        // Destination Register 2
+        // NOTE: this is only used for some SYSTEM instructions
+        //  where there's a single-cycle swap
+        .rd2_sel(),
+        .rd2_data
+        .rd2_wren(),
+    );
 
 endmodule
 
@@ -132,7 +248,7 @@ module riscv_cpu_alu(
             RISCV_RV32I_OPCODE_AUIPC,
             RISCV_RV32I_OPCODE_LUI: begin
                 byp = 32'h0000_0000;
-                res = op1 + (op2 << 12);            
+                res = op1 + (op2 << 12);
             end
             // System Instructions
             // op1 = operand 1
@@ -141,7 +257,7 @@ module riscv_cpu_alu(
             // res = result (to csr)
             // byp = result (to rd)
             RISCV_RV32I_OPCODE_SYSTEM: begin
-//TODO - implement system instructions            
+//TODO - implement system instructions
             end
             default: begin
 //TODO - implement bad instruction exception
